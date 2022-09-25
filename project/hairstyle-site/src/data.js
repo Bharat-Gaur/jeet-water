3e50060d28d788695b91395cd94e99866a0f83de
bharat <bharatiyamta@gmail.com>
2022-09-25 09:00:43 +0530

    first commit

---
+++ project/hairstyle-site/src/data.js
@@ -0,0 +1,96 @@
+// data.js
+const data = [
+    {
+      id: 1,
+      pic:
+      "../images/booster-panal.jpeg.jpeg",
+      name: "Water Booster Panal",
+      difficulty: "Medium",
+      info:
+        "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/"
+    },
+    {
+      id: 2,
+      pic:
+        "../images/cascade.jpg.jpg",
+      name: "Cascade",
+      difficulty: "Easy",
+      info:
+        "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/7/"
+    },
+    {
+      id: 3,
+      pic:
+        "../images/heat-pump.jpeg.jpeg",
+      name: "Heat Pump",
+      difficulty: "Hard",
+      info:
+        "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/12/"
+    },
+    {
+      id: 4,
+      pic:
+        "../images/pressure-panal.jpeg.jpeg",
+      name: "Pressure Pump Penal",
+      difficulty: "Medium",
+      info:
+        "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/54/"
+    },
+    {
+      id: 5,
+      pic:
+        "../images/softner.jpeg.jpeg",
+      name: "Water Softner",
+      difficulty: "Medium",
+      info:
+        "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/60/"
+    },
+    {
+      id: 6,
+      pic:
+        "../images/solor-gyser.jpeg.jpeg",
+      name: "Solor Gyser",
+      difficulty: "Easy",
+      info:
+        "https://therighthairstyles.com/20-most-inspiring-natural-hairstyles-for-short-hair/61/"
+    },
+    {
+      id: 7,
+      pic:
+        "../images/solor-panal.jpeg.jpeg",
+      name: "Solor Panal",
+      difficulty: "Medium",
+      info:
+        "https://www.goodhousekeeping.com/beauty/hair/g3536/natural-hairstyles/?slide=3"
+    },
+    {
+      id: 8,
+      pic:
+        "../images/vassel.jpeg.jpeg",
+      name: "Vassel",
+      difficulty: "Easy",
+      info:
+        "https://www.goodhousekeeping.com/beauty/hair/g3536/natural-hairstyles/?slide=10"
+    },
+    {
+      id: 9,
+      pic:
+        "../images/water-glass.jpg.jpg",
+      name: "Fresh Water",
+      difficulty: "Easy",
+      info:
+        "https://www.goodhousekeeping.com/beauty/hair/g3536/natural-hairstyles/?slide=12"
+    },
+    { id: 10,
+      pic: "../images/cascade.jpg.jpg",
+      name: "natural cascade",
+      difficulty: "Easy",
+      info: "",
+
+    },
+     
+    
+  ];
+  
+  export default data;
+  
